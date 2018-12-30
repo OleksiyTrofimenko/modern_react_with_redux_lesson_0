dd0e40a0bd208c52b7b6c56f97f87f5d103d3428
oleksiy <bakke87@gmail.com>
2018-12-30 17:16:43 +0200

    Lesson 0 - Update spinner and condition

--- src/seasons/index.js
+++ src/seasons/index.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import SeasonDisplay from './season-display';
+import Spinner from './spinner';
 
 class SeasonsApp extends React.Component {
-  // When class is defined, constructor method initialy called to define initial data
-  constructor(props) {
-    // Is a reference for parent components
-    super(props);
+  // // When class is defined, constructor method initialy called to define initial data
+  // constructor(props) {
+  //   // Is a reference for parent components
+  //   super(props);
 
-    // This is the only time when we do direct assignment
-    this.state = {
-      latitude: null,
-      errorMessage: '',
-    };
+  //   // This is the only time when we do direct assignment
+  //   this.state = {
+  //     latitude: null,
+  //     errorMessage: '',
+  //   };
+  // }
+
+  // This is the same result as we use this.state in constructor method.
+  state = {
+    latitude: null,
+    errorMessage: '',
+  };
+
+  componentDidMount() {
+    console.log('I am mounted once');
 
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
         // We called setstate to update our data!
-        this.setState({
-          latitude: position.coords.latitude,
-        });
-
+        (position) => this.setState({ latitude: position.coords.latitude }),
         // Never do this update! It's calling MUTATE
         // this.state.latitude = position.coords.latitude;
-      },
-      (err) => {
-        if (err) {
-          this.setState({
-            errorMessage: err.message,
-          });
-        }
-      }, 
+        (err) => this.setState({ errorMessage: err.message }),
     );
+
+    // console.log(window.navigator.geolocation.getCurrentPosition());
+
+    console.log(this.state);
   }
- 
-  // React says we have to define render!
-  render() {
+
+  componentDidUpdate() {
+    console.log('I am mounted several times');
+    console.log(this.state);
+  }
+
+  renderContent() {
     const { latitude, errorMessage } = this.state;
 
     if (errorMessage && !latitude) {
@@ -45,12 +53,21 @@ class SeasonsApp extends React.Component {
 
     if (!errorMessage && latitude) {
       return (
-        <div>Latitude: { latitude }</div>
+        <SeasonDisplay latitude={latitude} />
       );
     }
 
     return (
-      <div>Loading...</div>
+      <Spinner message="Please accept location request" />
+    );
+  }
+ 
+  // React says we have to define render!
+  render() {
+    return(
+      <div className="border red">
+        {this.renderContent()}
+      </div> 
     );
   }
 };
--- src/seasons/season-display.js
+++ src/seasons/season-display.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import './season-display.css';
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach',
+    icon: 'sun',
+  },
+  winter: {
+    text: 'Burr, it\'s chilly',
+    icon: 'snowflake',
+  },
+};
+
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? 'summer' : 'winter';
+  } else {
+    return latitude > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = ({ latitude }) => {
+  const season = getSeason(latitude, new Date().getMonth());
+  // const isWinter = season === 'winter';
+  // const text = isWinter ? 'Burr, it\'s chilly' : 'Let hit the beach';
+  // const icon = isWinter ? 'snowflake' : 'sun';
+  
+  const {text, icon} = seasonConfig[season];
+
   return (
-    <div className="ui container comments">
-      Season Display
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${icon} icon`} />
+      <h1>
+        {text}
+      </h1>
+      <i className={`icon-right massive ${icon} icon`} />
     </div>
   );
 };
